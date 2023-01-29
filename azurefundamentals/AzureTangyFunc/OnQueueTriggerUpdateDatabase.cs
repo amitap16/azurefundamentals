da0e1cc735f48181588e0f158c9ea486746bff33
Amita Patel <amitap16@live.com>
2023-01-29 17:24:56 -0500

    Replace from WebJobsStartup with FunctionsStartup

--- azurefundamentals/AzureTangyFunc/OnQueueTriggerUpdateDatabase.cs
+++ azurefundamentals/AzureTangyFunc/OnQueueTriggerUpdateDatabase.cs
@@ -1,17 +1,27 @@
-using System;
+using AzureTangyFunc.Data;
+using AzureTangyFunc.Models;
 using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
 
 namespace AzureTangyFunc
 {
     public class OnQueueTriggerUpdateDatabase
     {
+        private readonly AzureTangyDbContext _dbContext;
+
+        public OnQueueTriggerUpdateDatabase(AzureTangyDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
         [FunctionName("OnQueueTriggerUpdateDatabase")]
-        public void Run([QueueTrigger("SalesRequestInBound", Connection = "AzureWebJobsStorage")]string myQueueItem, 
+        public void Run([QueueTrigger("SalesRequestInBound", Connection = "AzureWebJobsStorage")] SalesRequest myQueueItem,
             ILogger log)
         {
             log.LogInformation($"C# Queue trigger function processed: {myQueueItem}");
+            myQueueItem.Status = "Submitted";
+            _dbContext.SalesRequests.Add(myQueueItem);
+            _dbContext.SaveChanges();
         }
     }
 }
--- azurefundamentals/AzureTangyFunc/Startup.cs
+++ azurefundamentals/AzureTangyFunc/Startup.cs
@@ -1,21 +1,21 @@
 ﻿using AzureTangyFunc;
 using AzureTangyFunc.Data;
-using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Hosting;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 
-[assembly: WebJobsStartup(typeof(Startup))]
+[assembly: FunctionsStartup(typeof(Startup))]
 namespace AzureTangyFunc
 {
-    public class Startup : IWebJobsStartup
+    public class Startup : FunctionsStartup
     {
-        public void Configure(IWebJobsBuilder builder)
+        public override void Configure(IFunctionsHostBuilder builder)
         {
             string connectionString = Environment.GetEnvironmentVariable("AzureSqlDatabase");
             builder.Services.AddDbContext<AzureTangyDbContext>(
-                options => options.UseSqlServer(connectionString));
+                options => SqlServerDbContextOptionsExtensions.UseSqlServer(options, connectionString)
+                );
         }
     }
 }
