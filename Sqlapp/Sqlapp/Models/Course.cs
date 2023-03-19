0f06cc9f9021042a09dfc423c8c3385c9f67b73d
Amita Patel <amitap16@live.com>
2023-03-19 16:55:55 -0400

    AZ-305 sql app

---
+++ Sqlapp/Sqlapp/Models/Course.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sqlapp.Models
+{
+    // This class represents the struture of our data
+    public class Course
+    {
+        public int CourseID { get; set; }
+
+        public string CourseName { get; set; }
+        public decimal Rating { get; set; }
+
+        public string ExamImage { get; set; }
+    }
+}
---
+++ Sqlapp/Sqlapp/Services/CourseService.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using Sqlapp.Models;
+
+namespace Sqlapp.Services
+{
+    public class CourseService
+    {
+        // Ensure to change the below variables to reflect the connection details for your database
+        private static string db_source = "az305demodbserver.database.windows.net";
+        private static string db_user = "demouser";
+        private static string db_password = "dem0user#1234";
+        private static string db_database = "az305demodb";
+
+        private SqlConnection GetConnection()
+        {
+            // Here we are creating the SQL connection
+            var _builder = new SqlConnectionStringBuilder();
+            _builder.DataSource = db_source;
+            _builder.UserID = db_user;
+            _builder.Password = db_password;
+            _builder.InitialCatalog = db_database;
+            return new SqlConnection(_builder.ConnectionString);
+        }
+
+        public IEnumerable<Course> GetCourses()
+        {
+            List<Course> _lst = new List<Course>();
+            string _statement = "SELECT CourseID,ExamImage,CourseName,rating from Course";
+            SqlConnection _connection = GetConnection();
+            // Let's open the connection
+            _connection.Open();
+            // We then construct the statement of getting the data from the Course table
+            SqlCommand _sqlcommand = new SqlCommand(_statement, _connection);
+            // Using the SqlDataReader class , we will read all the data from the Course table
+            using (SqlDataReader _reader = _sqlcommand.ExecuteReader())
+            {
+                while (_reader.Read())
+                {
+                    Course _course = new Course()
+                    {
+                        CourseID = _reader.GetInt32(0),
+                        ExamImage=_reader.GetString(1),
+                        CourseName = _reader.GetString(2),
+                        Rating = _reader.GetDecimal(3)
+                    };
+
+                    _lst.Add(_course);
+                }
+            }
+            _connection.Close();
+            return _lst;
+        }
+
+    }
+    }
+
+    
+
